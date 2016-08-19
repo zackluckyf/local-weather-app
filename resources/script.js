6aa0b32af8f6b5a23a94b20e47234a2feae54c9c
zackluckyf <fanning.34@gmail.com>
2016-08-19 01:24:45 -0400

    made secure

--- resources/script.js
+++ resources/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function() {
     "use strict";
-    
+
     // calls the openweathermap data by latitude and longitude
     // api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
-    
+
     var location = $(".location");
     var temperature = $(".temperature");
     var weather = $(".weather");
@@ -34,8 +34,8 @@ $(document).ready(function(){
 
     imgArray[5] = new Image();
     imgArray[5].src = 'resources/images/windy.png';
-    
-    
+
+
     function getLocation() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition);
@@ -43,70 +43,65 @@ $(document).ready(function(){
             location.html("Geolocation is not supported by this browser.");
         }
     }
-    
-    function latDirection(){
+
+    function latDirection() {
         latDisplay < 0 ? latLetter = "S" : latLetter = "N";
         longDisplay < 0 ? longLetter = "W" : longLetter = "E";
     }
-    
+
     function showPosition(position) {
         latitude = Math.round(position.coords.latitude);
         longitude = Math.round(position.coords.longitude);
-        latitude < 0 ? latDisplay = latitude * -1 : latDisplay = latitude * 1; 
-        longitude < 0 ? longDisplay = longitude * -1 : longDisplay = longitude * 1; 
+        latitude < 0 ? latDisplay = latitude * -1 : latDisplay = latitude * 1;
+        longitude < 0 ? longDisplay = longitude * -1 : longDisplay = longitude * 1;
         latDirection();
-        location.html("Latitude: " + latDisplay + "&deg; " + latLetter + "<br>Longitude: " + longDisplay + "&deg; " + longLetter); 
-        $.get( "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=1&appid=6b766b599bfc44b9f27b4f348f6ce770&units=imperial", function( data ) {
+        location.html("Latitude: " + latDisplay + "&deg; " + latLetter + "<br>Longitude: " + longDisplay + "&deg; " + longLetter);
+        $.get("https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&cnt=1&appid=6b766b599bfc44b9f27b4f348f6ce770&units=imperial", function(data) {
             temp = Math.round(data.main.temp);
             temperature.html(temp + "&deg; F");
             weather.html(data.weather[0].description);
-            if(data.weather[0].description.includes('clear')){
+            if (data.weather[0].description.includes('clear')) {
                 $('img').src = imgArray[0].src;
             }
             weatherPicture(data);
-            }, "json" );
+            console.log(data);
+        }, "json");
     }
-    
-    function weatherPicture(data){
-        if(data.weather[0].description.includes('clear')){
+
+    function weatherPicture(data) {
+        if (data.weather[0].description.includes('clear')) {
             document.getElementById("imageid").src = imgArray[0].src;
-        }
-        else if(data.weather[0].description.includes('cloudy')){
+        } else if (data.weather[0].description.includes('cloudy')) {
             document.getElementById("imageid").src = imgArray[1].src;
-        }
-        else if(data.weather[0].description.includes('rain')){
+        } else if (data.weather[0].description.includes('rain')) {
             document.getElementById("imageid").src = imgArray[2].src;
-        }
-        else if(data.weather[0].description.includes('snow')){
+        } else if (data.weather[0].description.includes('snow')) {
             document.getElementById("imageid").src = imgArray[3].src;
-        }
-        else if(data.weather[0].description.includes('thunder')){
+        } else if (data.weather[0].description.includes('thunder')) {
             document.getElementById("imageid").src = imgArray[4].src;
-        }
-        else if(data.weather[0].description.includes('windy')){
+        } else if (data.weather[0].description.includes('windy')) {
             document.getElementById("imageid").src = imgArray[5].src;
         }
-        
+
     }
-    
+
     getLocation();
-    
-    temperature.click(function(){
-        if(metric === true){
+
+    temperature.click(function() {
+        if (metric === true) {
             metric = false;
-            $.get( "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=6b766b599bfc44b9f27b4f348f6ce770&units=metric", function( data ) {
+            $.get("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=6b766b599bfc44b9f27b4f348f6ce770&units=metric", function(data) {
                 temp = Math.round(data.main.temp);
                 temperature.html(temp + "&deg; C");
-                }, "json" );
-            }
-        else{
+            }, "json");
+        } else {
             metric = true;
-            $.get( "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=6b766b599bfc44b9f27b4f348f6ce770&units=imperial", function( data ) {
+            $.get("https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=6b766b599bfc44b9f27b4f348f6ce770&units=imperial", function(data) {
                 temp = Math.round(data.main.temp);
                 temperature.html(temp + "&deg; F");
-                }, "json" );    
+            }, "json");
         }
-        
+
     });
-    
+
 });
